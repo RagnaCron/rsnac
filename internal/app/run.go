6f2d7b13f1b9d8a9eaf92a2fea222c34d0be2c47
Manuel Werder <manuelwerder@hotmail.com>
2026-05-15 13:04:38 +0200

    Adding app run package

---
+++ internal/app/run.go
@@ -0,0 +1,12 @@
+// Package app
+package app
+
+import (
+	"errors"
+
+	"github.com/RagnaCron/rsnac/internal/config"
+)
+
+func Run(path string, cfg config.Config) error {
+	return errors.New("not implemented")
+}
