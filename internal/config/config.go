362ddf2ad406935bd8ca71cb8f788a2a8e765ab3
Manuel Werder <manuelwerder@hotmail.com>
2026-05-15 13:27:48 +0200

    Minimal Load Config implementation

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,23 @@
 // Package config
 package config
 
-import "errors"
+import (
+	"fmt"
+	"os"
+)
 
 type Config struct {
 	Path string
 }
 
-func Load() (Config, error) {
-	return Config{}, errors.New("config Load not implemented")
+func Load() (*Config, error) {
+	if len(os.Args[1:]) != 1 {
+		return &Config{}, fmt.Errorf("error: pass 1 argument, got: %d", len(os.Args[1:]))
+	}
+
+	cfg := &Config{
+		Path: os.Args[1],
+	}
+
+	return cfg, nil
 }
