fd297ab4ea7cfab8298ad26b679ac779e8c35b25
Manuel Werder <manuelwerder@hotmail.com>
2026-05-15 13:03:56 +0200

    Adding error to config for not yet implemented

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 // Package config
 package config
 
+import "errors"
+
 type Config struct {
 	Path string
+}
+
+func Load() (Config, error) {
+	return Config{}, errors.New("config Load not implemented")
 }
