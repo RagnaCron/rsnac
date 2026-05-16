f3b75e703a2ed35b59386cbd8561c9b193c31285
Manuel Werder <manuelwerder@hotmail.com>
2026-05-16 14:43:31 +0200

    Simplifing rename package, just renames oldpath to newpath, no tests, as we do not test stdlib

--- internal/rename/rename.go
+++ internal/rename/rename.go
@@ -7,23 +7,8 @@ import (
 )
 
 func Rename(oldPath, newPath string) error {
-	oStat, err := os.Lstat(oldPath)
-	if err != nil {
-		return fmt.Errorf("error while reading lstat from: %q, with: %w", oldPath, err)
-	}
-	nStat, err := os.Lstat(newPath)
-	if err != nil {
-		return fmt.Errorf("error while reading lstat from: %q, with: %w", newPath, err)
-	}
-
-	if oStat.Name() == nStat.Name() {
-		return fmt.Errorf("collision: %q already exists", newPath)
-	}
-
-	err = os.Rename(oldPath, newPath)
-	if err != nil {
+	if err := os.Rename(oldPath, newPath); err != nil {
 		return fmt.Errorf("rename %q -> %q: %w", oldPath, newPath, err)
 	}
-
 	return nil
 }
